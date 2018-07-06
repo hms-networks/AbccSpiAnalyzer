668eb5a75c88de9c1e04f0caf6fa2822dac75503
Jon Carrier <joca@hms-networks.com>
2018-07-06 16:07:04 -0400

    Updated abcc_td.h to sync better with LogicPublicTypes.h
    And to remove old defines that have no place/purpose in this
    project.

--- source/abcc_td.h
+++ source/abcc_td.h
@@ -1,14 +1,5 @@
 /*******************************************************************************
 ********************************************************************************
-**                                                                            **
-** ABCC Starter Kit version 2.01.01 (2015-12-14)                              **
-**                                                                            **
-** Delivered with:                                                            **
-**    ABCC Driver 4.01.01 (2015-12-14)                                        **
-**    ABP         7.16.01 (2015-10-14)                                        **
-**                                                                            */
-/*******************************************************************************
-********************************************************************************
 ** COPYRIGHT NOTIFICATION (c) 2015 HMS Industrial Networks AB                 **
 **                                                                            **
 ** This code is the property of HMS Industrial Networks AB.                   **
@@ -30,6 +21,7 @@
 
 #ifndef ABCC_TD_H
 #define ABCC_TD_H
+
 /*******************************************************************************
 **
 ** Constants
@@ -59,66 +51,52 @@
 ** The standard unsigned 8, 16, and 32 bit data types (respectively).
 **---------------------------------------------------------------------------
 */
-
-/* workaround necessary for IAR AVR compiler            */
-/* e.g. "UINT8 *" requires "__generic" keyword to point */
-/* to ROM and RAM                                       */
-#define GENPTR
-
-#ifndef BOOLEAN
-  #ifdef bool   /* E.g. by stdbool.h */
-    #define  BOOLEAN  bool
-  #else
-    typedef unsigned char   BOOLEAN;
-  #endif
-#endif
-
 #ifndef BOOL
-typedef int             BOOL;
+typedef int                       BOOL;
 #endif
 
 #ifndef BOOL8
-typedef unsigned char   BOOL8;
+typedef unsigned char             BOOL8;
 #endif
 
 #ifndef CHAR
-typedef unsigned char   CHAR;
+typedef unsigned char             CHAR;
 #endif
 
 #ifndef UINT8
-typedef unsigned char   UINT8;
+typedef unsigned char             UINT8;
 #endif
 
 #ifndef INT8
-typedef signed char     INT8;
+typedef signed char               INT8;
 #endif
 
 #ifndef UINT16
-typedef unsigned short  UINT16;
+typedef unsigned short            UINT16;
 #endif
 
 #ifndef INT16
-typedef signed short    INT16;
+typedef signed short              INT16;
 #endif
 
 #ifndef UINT32
-typedef unsigned int    UINT32;
+typedef unsigned int              UINT32;
 #endif
 
 #ifndef INT32
-typedef signed int      INT32;
+typedef signed int                INT32;
 #endif
 
 #ifndef FLOAT32
-typedef float           FLOAT32;
+typedef float                     FLOAT32;
 #endif
 
 #ifndef UINT64
-typedef unsigned long long    UINT64;
+typedef unsigned long long int    UINT64;
 #endif
 
 #ifndef INT64
-typedef signed long long    INT64;
+typedef signed long long int      INT64;
 #endif
 
 /*------------------------------------------------------------------------------
@@ -177,10 +155,6 @@ typedef UINT32    BeUINT32;
 */
 #ifndef NULL
 #define NULL 0
-#endif
-
-#ifndef NULLCLBK
- #define NULLCLBK (void*)0L
 #endif
 
 #ifdef __cplusplus
